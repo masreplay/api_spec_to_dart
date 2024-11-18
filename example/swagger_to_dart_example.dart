6fc625471cc702d0f79361c381ae99c5d3055017
Mohammed Atheer <mohammed.atheer@ta3leem.dev>
2024-11-18 11:21:58 +0300

    first commit

---
+++ example/swagger_to_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:swagger_to_dart/swagger_to_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/swagger_to_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:swagger_to_dart/swagger_to_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
