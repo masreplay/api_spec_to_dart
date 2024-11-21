3a4eab85a563b826f3ca2f8314bf46e694979a45
Mohammed Atheer <mohammed.atheer@ta3leem.dev>
2024-11-21 18:58:17 +0300

    Add YAML package support to load package name from pubspec.yaml in swagger_to_dart

--- bin/swagger_to_dart.dart
+++ bin/swagger_to_dart.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:path/path.dart' as path;
 import 'package:logger/logger.dart';
+import 'package:yaml/yaml.dart';
 import 'package:swagger_to_dart/src/config/open_api_generator_config.dart';
 import 'package:swagger_to_dart/src/generator/openapi/open_api_generator.dart';
 
@@ -10,8 +11,19 @@ final logger = Logger();
 Future<void> main(List<String> args) async {
   final String dir = Directory.current.path;
 
+  final pubspecYamlFile = File(path.join(dir, 'pubspec.yaml'));
+
+  if (!pubspecYamlFile.existsSync()) {
+    logger.e('pubspec.yaml not found in the current directory');
+    exit(1);
+  }
+
+  final pubspecYaml = loadYaml(pubspecYamlFile.readAsStringSync());
+
+  final packageName = pubspecYaml['name'];
+
   final config = OpenApiGeneratorConfig(
-    packageName: 'example',
+    packageName: packageName,
     input: path.join(dir, 'schema/swagger.json'),
     output: path.join(dir, 'lib/src/gen'),
     isFlutter: false,
