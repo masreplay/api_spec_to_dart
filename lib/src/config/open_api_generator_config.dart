fc65498177e4fede0bc14fd766ebbb9bc0aa2315
Mohammed Atheer <mohammed.atheer@ta3leem.dev>
2024-11-25 10:13:14 +0300

    Change return type for OpenApiSchemaVarType.number from 'float' to 'double'

--- lib/src/config/open_api_generator_config.dart
+++ lib/src/config/open_api_generator_config.dart
@@ -47,7 +47,7 @@ class SwaggerToDartConfig {
           _ => 'String',
         };
       case OpenApiSchemaVarType.number:
-        return 'float';
+        return 'double';
       case OpenApiSchemaVarType.integer:
         return 'int';
       case OpenApiSchemaVarType.boolean:
