ccc5f01e9703ecc753b1b1d89be0f0ad83996534
Mohammed Atheer <mohammed.atheer@ta3leem.dev>
2025-05-13 22:25:20 +0300

    refactor: Reintroduce swagger_to_dart_yaml.dart export in config.dart for improved module accessibility and maintainability

--- packages/swagger_to_dart/lib/src/config/config.dart
+++ packages/swagger_to_dart/lib/src/config/config.dart
@@ -1,3 +1,3 @@
-export 'swagger_to_dart_yaml.dart';
 export 'generation_context.dart';
+export 'swagger_to_dart_yaml.dart';
 export 'open_api_to_dart_type_converter.dart';
---
+++ packages/swagger_to_dart/lib/src/generator/generator.dart
@@ -0,0 +1,4 @@
+export 'api_client/api_client.dart';
+export 'code_generator.dart';
+export 'generation_context_builder.dart';
+export 'model/model.dart';
