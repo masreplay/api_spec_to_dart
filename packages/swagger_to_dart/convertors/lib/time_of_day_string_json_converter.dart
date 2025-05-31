8ae3a099b56f56a5cc3e7ecc97ff9db5c2828850
masreplay <masreplay@gmail.com>
2025-05-31 16:20:18 +0300

    fix: Enhance TimeOfDayStringJsonConverter to handle parsing errors gracefully and improve time formatting

--- packages/swagger_to_dart/convertors/lib/time_of_day_string_json_converter.dart
+++ packages/swagger_to_dart/convertors/lib/time_of_day_string_json_converter.dart
@@ -4,27 +4,39 @@ import 'package:json_annotation/json_annotation.dart';
 class TimeOfDayStringJsonConverter implements JsonConverter<TimeOfDay, String> {
   const TimeOfDayStringJsonConverter();
 
+  // 1:30:00 -> 1:30
+  // PT1H30M -> 1:30:00
   @override
   TimeOfDay fromJson(String json) {
     if (json.contains(':')) {
       final time = json.split(':');
-      return TimeOfDay(hour: int.parse(time[0]), minute: int.parse(time[1]));
+      return TimeOfDay(
+        hour: int.tryParse(time[0]) ?? 0,
+        minute: int.tryParse(time[1]) ?? 0,
+      );
     } else {
       final regex = RegExp(r'PT(?:(\d+)H)?(?:(\d+)M)?');
       final match = regex.firstMatch(json);
 
-      int hours = match?.group(1) != null ? int.parse(match!.group(1)!) : 0;
-      int minutes = match?.group(2) != null ? int.parse(match!.group(2)!) : 0;
+      final hours =
+          match?.group(1) != null ? int.tryParse(match!.group(1)!) ?? 0 : 0;
+      final minutes =
+          match?.group(2) != null ? int.tryParse(match!.group(2)!) ?? 0 : 0;
 
-      return TimeOfDay(hour: hours, minute: minutes);
+      return TimeOfDay(
+        hour: hours,
+        minute: minutes,
+      );
     }
   }
 
+  // 1:30 -> 1:30:00
   @override
   String toJson(TimeOfDay object) {
     final hour = object.hour.toString().padLeft(2, '0');
     final minute = object.minute.toString().padLeft(2, '0');
     final second = '00';
+
     return '$hour:$minute:$second';
   }
 }
