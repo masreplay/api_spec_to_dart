8270ca937313ca02ad9f186f00c9fc7ef1d37e2b
AliAmmar <aliammarali125@gmail.com>
2025-06-01 09:57:15 +0300

    feat: add custom config file path support

--- packages/swagger_to_dart/bin/swagger_to_dart.dart
+++ packages/swagger_to_dart/bin/swagger_to_dart.dart
@@ -1,10 +1,15 @@
 import 'dart:io';
-
+import 'package:args/args.dart';
 import 'package:swagger_to_dart/swagger_to_dart.dart';
 
-Future<void> main() async {
+Future<void> main(List<String> arguments) async {
   try {
-    final builder = GenerationContextBuilder();
+    final parser = ArgParser()
+      ..addOption('config', abbr: 'c', help: 'Path to config file');
+    final argResults = parser.parse(arguments);
+    final configPath = argResults['config'] as String?;
+
+    final builder = GenerationContextBuilder(configPath: configPath);
     final GenerationContext context = await builder.build();
 
     final generator = SwaggerToDartCodeGenerator(context);
